28a053648818376d65494b5b3fca66aaf042280d
Special <68615429+The-Special@users.noreply.github.com>
2021-04-05 19:05:14 +0300

    Update setup.py

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ classifiers = [
  
 setup(
   name='burclar',
-  version='1.0.1',
+  version='1.0.2',
   description='Basit bir burç modülü.',
   long_description_content_type="text/markdown",
   long_description=open('README.md').read(),
