b3de570b934cab37699f36201a0c6f2b750decfb
Special <68615429+The-Special@users.noreply.github.com>
2021-04-07 09:03:43 +0300

    Update __init__.py

--- burclar/__init__.py
+++ burclar/__init__.py
@@ -17,13 +17,14 @@ def makeAPIRequest(path: str, type: str) -> str:
     burcYorum = burc.text
     return burcYorum 
 
+
 burcList = [ "yengec", "koc", "boga", "ikizler", "aslan", "basak", "terazi", "akrep", "yay", "oglak", "kova", "balik" ]
 burcStr = ",".join(burcList)
 
 class burclar:
     def burc(name: str, type = "gunluk") -> str:
         if name not in burcList:
-            raise Exception(f"Geçerli bir buç girin. ({burcStr})")
+            raise Exception(f"Geçerli bir burç giriniz. ({burcStr})")
         return makeAPIRequest(name, type)
 
     def yengec(type = "gunluk") -> str:
@@ -61,3 +62,64 @@ class burclar:
 
     def balik(type = "gunluk") -> str:
         return makeAPIRequest("balik", type)
+    
+def makeAPIRequestOz(path: str , type: str) -> str:
+    type = type if (type == "ozellikleri") else "ozellikleri"
+    y = requests.get(f"https://www.mynet.com/kadin/burclar-astroloji/{path}-burcu-ozellikleri.html")
+    soupOz = BeautifulSoup(y.content , "html.parser")
+    dataOz = soupOz.find_all("div" , {"class":"medyanet-content"})
+    burcOz = (dataOz[0].contents) [len(dataOz[0].contents) -12]
+    burcYorumOz = burcOz.text
+    return burcYorumOz
+
+
+class burclar_oz:
+    def burcOz(name: str, type = "ozellikleri") -> str:
+        if name not in burcList:
+            raise Exception(f"Geçerli bir burç giriniz. ({burcStr})")
+        return makeAPIRequestOz(name, type)
+
+    def yengecOz(type = "ozellikleri") -> str:
+        return makeAPIRequestOz("yengec", type)
+
+    def kocOz(type = "ozellikleri") -> str:
+        return makeAPIRequestOz("koc", type)
+
+    def bogaOz(type = "ozellikleri") -> str:
+        return makeAPIRequestOz("boga", type)
+
+    def ikizlerOz(type = "ozellikleri") -> str:
+        return makeAPIRequestOz("ikizler", type)
+
+    def aslanOz(type = "ozellikleri") -> str:
+        return makeAPIRequestOz("aslan", type)
+
+    def basakOz(type = "ozellikleri") -> str:
+        return makeAPIRequestOz("basak", type)
+
+    def teraziOz(type = "ozellikleri") -> str:
+        return makeAPIRequestOz("terazi", type)
+
+    def akrepOz(type = "ozellikleri") -> str:
+        return makeAPIRequestOz("akrep", type)
+
+    def yayOz(type = "ozellikleri") -> str:
+        return makeAPIRequestOz("yay", type)
+
+    def oglakOz(type = "ozellikleri") -> str:
+        return makeAPIRequestOz("oglak", type)
+
+    def kovaOz(type = "ozellikleri") -> str:
+        return makeAPIRequestOz("kova", type)
+
+    def balikOz(type = "ozellikleri") -> str:
+        return makeAPIRequestOz("balik", type)
+
+
+
+
+
+
+
+
+
